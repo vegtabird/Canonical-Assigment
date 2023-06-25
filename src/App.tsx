4d4b17a11fc8d412f28ef8355671532d118940d7
whmiao <wenhuimiao94@gmail.com>
2023-06-25 22:13:02 +0800

    feat:add loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { useEffect, useRef } from 'react';
 import { BlogPostCard, IContent } from './components'
 import { useGetData } from './request/data';
 function App() {
-  const { data, sendRequest } = useGetData()
+  const { data, sendRequest, isLoading } = useGetData()
   const requestRef = useRef(sendRequest)
   useEffect(() => {
     requestRef.current()
   }, [])
   return (
     <div style={{ marginTop: '25px' }}>
-      <div className='row'>
-        {
-          data?.map((content, index) => {
-            return <BlogPostCard className='col-4' content={content as IContent} key={index} />
-          })
-        }
-      </div>
+      {
+        isLoading ? <div>Loading....</div> : <div className='row'>
+          {
+            data?.map((content, index) => {
+              return <BlogPostCard className='col-4' content={content as IContent} key={index} />
+            })
+          }
+        </div>
+      }
     </div>
   )
 }
--- src/request/data.ts
+++ src/request/data.ts
@@ -56,6 +56,7 @@ export function useGetData() {
                 payload: Array.isArray(res?.data) ? res.data : []
             })
         }).catch(error => {
+            alert('fail to fetch data')
             console.log(error)
         })
     }, [])
